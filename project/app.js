c6a82601a881448e134c4e0bcda45f73b3bd206b
Aqib Mughal <114867804+AqibMughal1@users.noreply.github.com>
2024-05-21 07:20:39 +0500

    updated main file

--- project/app.js
+++ project/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const ejsMate = require('ejs-mate');
+const route1 = require("./routes/route1");
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use("/", route1);
 
 
 app.listen(3000, (req, res) => {
