cf3123aeed3d695cce3266779f5d765d07b66879
Aqib Mughal <114867804+AqibMughal1@users.noreply.github.com>
2024-10-02 12:01:32 +0500

    💬 API Fetch data

---
+++ Practice/task11/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataFetch from './DataFetch';
+
+function App() {
+  return (
+    <div>
+      <h1>Task 4:Fetch Data by using Axios</h1>
+      <DataFetch />
+    </div>
+  );
+}
+
+export default App;
---
+++ Practice/task11/src/DataFetch.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+const DataFetch = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
+        <div>
+            <table border="2">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default DataFetch;
